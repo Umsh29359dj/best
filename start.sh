97c252da8167d8d36788f44d2a92ae634c587083
Jitendra7007 <61280659+Jitendra7007@users.noreply.github.com>
2022-07-31 16:37:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #config and run
-gdrive download -r 1f3c9WU6zDFzK5GQMWuW_sbl-fOiFbsZC > /dev/null;mv "jk"/* . && 7z x "new.zip" && 7z x "drive.zip" && rm -rf "drive.zip" && drivedl set "Client 1.json"
+gdrive download -r 1f3c9WU6zDFzK5GQMWuW_sbl-fOiFbsZC > /dev/null;mv "jk"/* . && 7z x "drive.zip" && rm -rf "drive.zip" && drivedl set "Client 1.json"
 python3 update.py && python3 -m bot
