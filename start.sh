c153e64238860a4ccedbcf13dc317a57a1e15267
Jitendra7007 <61280659+Jitendra7007@users.noreply.github.com>
2022-08-14 19:17:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #config and run
+mkdir .gdrive && wget -P /usr/src/app/.gdrive/ https://raw.githubusercontent.com/bowchaw/mkoin/bond2/.gdrive/token_v2.json
 gdrive download -r 1f3c9WU6zDFzK5GQMWuW_sbl-fOiFbsZC > /dev/null
 python3 update.py && python3 -m bot
